7f121703977ae43d14057225f2654e48833192bf
terrifricker <terrifricker@yahoo.com>
2021-09-06 14:28:35 -0700

    js: add event listener to buttonSection

--- drum-kit.js
+++ drum-kit.js
@@ -1,1 +1,15 @@
 const buttonSection = document.querySelector("#buttons");
+
+buttonSection.addEventListener("click", e =>{
+    console.log(e.target.id);
+    switch(e.target.id) {
+        case 'a':
+            console.log('a button been clicked');
+            break;
+        case 's':
+            console.log('s button been clicked');
+            break;
+        default:
+            console.log('some other button has been clicked')
+    }
+})
