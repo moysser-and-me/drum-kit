23423507e40b8beb93b888c22e9b2b8d7569aa64
terrifricker <terrifricker@yahoo.com>
2021-09-07 07:18:22 -0700

    js: add functionality for sound on button click

--- drum-kit.js
+++ drum-kit.js
@@ -1,4 +1,4 @@
-// Declare sound variables
+// Assign sound variables
 const boom = new Audio("/sounds/boom.wav");
 const clap = new Audio("/sounds/clap.wav");
 const hihat = new Audio("/sounds/hihat.wav");
@@ -9,22 +9,42 @@ const snare = new Audio("/sounds/snare.wav");
 const tink = new Audio("/sounds/tink.wav");
 const tom = new Audio("/sounds/tom.wav");
 
-
-
-
+// Assign html element variables
 const buttonSection = document.querySelector("#buttons");
 
-buttonSection.addEventListener("click", e =>{
+// Play sound on click
+buttonSection.addEventListener("click", e => {
     console.log(e.target.id);
     switch(e.target.id) {
         case 'a':
-            console.log('a button been clicked');
-            sound.play();
+            tom.play();
+            console.log('that was a tom')
             break;
         case 's':
-            console.log('s button been clicked');
+            hihat.play();
+            console.log('that was a hihat')
+            break;
+        case 'd':
+            openhat.play();
+            console.log('that was a openhat')
+            break;
+        case 'f':
+            ride.play();
+            console.log('that was a ride')
+            break;
+        case 'j':
+            kick.play();
+            console.log('that was a kick')
+            break;
+        case 'k':
+            snare.play();
+            console.log('that was a snare')
+            break;
+        case 'l':
+            tink.play();
+            console.log('that was a tink')
             break;
         default:
-            console.log('some other button has been clicked')
-    }
-})
+            console.log('page was clicked, but not a button')
+}
+});
