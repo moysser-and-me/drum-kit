3de44fbb0025c76e83775e4c30b76bfe885ecba2
terrifricker <terrifricker@yahoo.com>
2021-09-07 06:54:58 -0700

    js: declare sound variables

--- drum-kit.js
+++ drum-kit.js
@@ -1,3 +1,17 @@
+// Declare sound variables
+const boom = new Audio("/sounds/boom.wav");
+const clap = new Audio("/sounds/clap.wav");
+const hihat = new Audio("/sounds/hihat.wav");
+const kick = new Audio("/sounds/kick.wav");
+const openhat = new Audio("/sounds/openhat.wav");
+const ride = new Audio("/sounds/ride.wav");
+const snare = new Audio("/sounds/snare.wav");
+const tink = new Audio("/sounds/tink.wav");
+const tom = new Audio("/sounds/tom.wav");
+
+
+
+
 const buttonSection = document.querySelector("#buttons");
 
 buttonSection.addEventListener("click", e =>{
@@ -5,6 +19,7 @@ buttonSection.addEventListener("click", e =>{
     switch(e.target.id) {
         case 'a':
             console.log('a button been clicked');
+            sound.play();
             break;
         case 's':
             console.log('s button been clicked');
